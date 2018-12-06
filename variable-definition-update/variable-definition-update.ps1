88383dc700bf673ef90020012ce989d59300fb2e
Bastien Perdriau <bastien.perdriau@live.fr>
2018-12-06 23:04:05 +0100

    Extension initialized

---
+++ variable-definition-update/variable-definition-update.ps1
@@ -0,0 +1,27 @@
+#
+# variable-definition-update.ps1
+#
+[CmdletBinding(DefaultParameterSetName = 'None')]
+param(
+	[string][Parameter(Mandatory=$true)] $variable1, 
+	[string] $variable2
+)
+
+Write-Host "Starting variable-definition-update"
+
+#Run Save-Module -Name VstsTaskSdk -Path .\ for get the Powershell VSTS SDK
+# see https://github.com/Microsoft/vsts-task-lib/tree/master/powershell/Docs
+#Trace-VstsEnteringInvocation $MyInvocation
+
+try {
+Write-Host "variable 1: "$variable1
+Write-Host "variable 2: "$variable2
+
+
+} catch {
+
+} finally {
+	#Trace-VstsLeavingInvocation $MyInvocation
+}
+
+Write-Host "Ending variable-definition-update"
