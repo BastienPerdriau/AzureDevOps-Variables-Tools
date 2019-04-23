4fce065c955f8eee218b0a40ce94b10350354b38
Bastien Perdriau <bastien.perdriau@live.fr>
2019-04-23 23:27:57 +0200

    Gestion des versions de tâches

---
+++ build.ps1
@@ -0,0 +1,22 @@
+$tasks = Get-ChildItem -Filter "*Task" -Directory
+
+$manifest = Get-Content -Path ".\vss-extension.json" -Force | ConvertFrom-Json
+
+$version = $manifest.version
+$versionParts = $version.Split(".")
+
+$majorVersion = $versionParts[0]
+$minorVersion = $versionParts[1]
+$patchVersion = $versionParts[2]
+
+foreach ($task in $tasks)
+{
+    $taskManifestPath = Join-Path -Path $task.FullName -ChildPath "task.json"
+    $taskManifest = Get-Content -Path $taskManifestPath -Force | ConvertFrom-Json
+
+    $taskManifest.version.Major = $majorVersion
+    $taskManifest.version.Minor = $minorVersion
+    $taskManifest.version.Patch = $patchVersion
+
+    $taskManifest | ConvertTo-Json -Depth 3 | Set-Content -Path $taskManifestPath -Force
+}
