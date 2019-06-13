f850c936508c08c91397ea08b9a005364038876f
Bastien PERDRIAU <b.perdriau@gest-innov.fr>
2019-06-13 23:19:48 +0200

    Merge Increment and Decrement tasks

---
+++ IntegerVariableOperationTask/IntegerVariableOperationTask.ps1
@@ -0,0 +1,25 @@
+Trace-VstsEnteringInvocation $MyInvocation
+
+$VariableName = Get-VstsInput -Name variableName -Require
+$Operation = Get-VstsInput -Name operation -Require
+$Step = Get-VstsInput -Name step -Require
+
+$oldValueString = (Get-Item env:$VariableName).Value
+[int]$oldValue = [convert]::ToInt32($oldValueString, 10)
+
+switch ($Operation) {
+    "increment"
+    {
+        $newValue = $oldValue + $Step
+    }
+    "decrement"
+    {
+        $newValue = $oldValue - $Step
+    }
+    Default {}
+}
+
+Write-Host ("##vso[task.setvariable variable={0};]{1}" -f $VariableName, $newValue)
+Write-Output ("Variable '{0}' is updated from '{1}' to '{2}'" -f $VariableName, $oldValue, $newValue)
+
+Trace-VstsLeavingInvocation $MyInvocation
