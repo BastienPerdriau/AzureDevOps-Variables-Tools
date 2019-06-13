5ba9c3a236b099b3bf46e8075e7122253a6e0f57
Bastien PERDRIAU <b.perdriau@gest-innov.fr>
2019-06-13 23:40:41 +0200

    Adding source variable and integer operations

--- VariableDefinitionUpdateTask/GetDefinitionVariableValue.psm1
+++
@@ -1,5 +0,0 @@
-Trace-VstsEnteringInvocation $MyInvocation
-
-
-
-Trace-VstsLeavingInvocation $MyInvocation
--- VariableDefinitionUpdateTask/VariableDefinitionUpdateTask.ps1
+++ VariableDefinitionUpdateTask/VariableDefinitionUpdateTask.ps1
@@ -2,77 +2,92 @@ Trace-VstsEnteringInvocation $MyInvocation
 
 [string]$PAT = Get-VstsInput -Name PAT -Require
 [string]$VariableName = Get-VstsInput -Name variableName -Require
-[string]$VariableValue = Get-VstsInput -Name variableValue -Require
+[string]$UseValueFrom = Get-VstsInput -Name useValueFrom -Require
+[string]$OutputNewValue = Get-VstsInput -Name outputNewValue -AsBool
 
-# TODO Use VSS Web Extension SDK to authenticate instead of token https://docs.microsoft.com/en-us/rest/api/azure/devops/?view=azure-devops-rest-5.0
-# TODO Add option to this task to set either
-# - a hard value
-# - a variable name to use value
+$ApiVersion="5.0"
+$uriRoot = $env:SYSTEM_TEAMFOUNDATIONSERVERURI
+$ProjectName = $env:SYSTEM_TEAMPROJECT
+$BuildId = $env:SYSTEM_DEFINITIONID 
+$uri = "$uriRoot$ProjectName/_apis/build/definitions?api-version=$ApiVersion"
 
-# - option to take variable from definition
-# - option to take variable from running build
+# Base64-encodes the Personal Access Token (PAT) appropriately
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "", $PAT)))
+$headers = @{Authorization = ("Basic {0}" -f $base64AuthInfo)}
 
-# - option to increment the value
-# - option to decrement the value
-# - option to write the new value into env variables
+# Get the list of Build Definitions
+$buildDefs = Invoke-RestMethod -Uri $uri -Method Get -ContentType "application/json" -Headers $headers
 
-try
+# Find the build definition for this project
+$buildDef = $buildDefs.value | Where-Object { $_.id -eq $BuildId }
+if ($null -eq $buildDef)
 {
-	$ApiVersion="5.0"
-	$uriRoot = $env:SYSTEM_TEAMFOUNDATIONSERVERURI
-	$ProjectName = $env:SYSTEM_TEAMPROJECT
-	$BuildId = $env:SYSTEM_DEFINITIONID 
-	$uri = "$uriRoot$ProjectName/_apis/build/definitions?api-version=$ApiVersion"
-
-	# Base64-encodes the Personal Access Token (PAT) appropriately
-	$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "", $PAT)))
-	$header = @{Authorization = ("Basic {0}" -f $base64AuthInfo)}
-
-	# Get the list of Build Definitions
-	$buildDefs = Invoke-RestMethod -Uri $uri -Method Get -ContentType "application/json" -Headers $header
-
-	# Find the build definition for this project
-	$buildDef = $buildDefs.value | Where-Object { $_.id -eq $BuildId }
-	if ($null -eq $buildDef)
-	{
-		Write-Error "Unable to find a build definition for Project '$ProjectName'. Check the config values and try again." -ErrorAction Stop
-	}
-
-	$getUrl = "$($buildDef.Url)?api-version=$ApiVersion"
-	$projectDef = Invoke-RestMethod -Uri $getUrl -Method Get -ContentType "application/json" -Headers $header
-
-	if ($null -eq $projectDef.variables.$VariableName)
-	{
-		Write-Error "Unable to find a variable called '$VariableName' in Project $ProjectName. Please check the config and try again." -ErrorAction Stop
-	}
-
-	# Update the value and update Azure DevOps
-	$projectDef.variables.$VariableName.Value = $VariableValue
-	$projectDefJson = $projectDef | ConvertTo-Json -Depth 50 -Compress
-	Write-Output ("projectDef.variables.VariableName.Value" -f $projectDef.variables.$VariableName.Value)
-
-	# build the URL to cater for if the Project Definition URL already has parameters or not.
-	$separator = "?"
-	if ($projectDef.Url -like '*?*')
-	{
-		$separator = "&"
-	}
-	$putUrl = "$($projectDef.Url)$($separator)api-version=$ApiVersion"
-	Write-Verbose "Updating Project Build number with URL: $putUrl"
-	Write-Output $projectDef.variables
-	Write-Output (ConvertFrom-Json $projectDefJson)
-	Write-Output "--------------------------"
-	Invoke-RestMethod -Method Put -Uri $putUrl -Headers $header -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($projectDefJson)) | Out-Null
-
-	Write-Host "##vso[task.setvariable variable=$VariableName]$VariableValue"
-}
-catch
-{
-
-}
-finally
-{
-	Trace-VstsLeavingInvocation $MyInvocation
+	Write-Error "Unable to find a build definition for Project '$ProjectName'. Check the config values and try again." -ErrorAction Stop
 }
 
-Write-Host "Ending variable-definition-update"
+$getUrl = "$($buildDef.Url)?api-version=$ApiVersion"
+$projectDef = Invoke-RestMethod -Uri $getUrl -Method Get -ContentType "application/json" -Headers $headers
+
+if ($null -eq $projectDef.variables.$VariableName)
+{
+	Write-Error "Unable to find a variable called '$VariableName' in Project $ProjectName. Please check the config and try again." -ErrorAction Stop
+}
+
+switch ($UseValueFrom) {
+	"pipelineDefinition"
+	{
+		$oldValue = $projectDef.variables.$VariableName.Value
+	}
+	"pipelineExecution"
+	{
+		$oldValue = (Get-Item env:$VariableName).Value
+	}
+	{($_ -eq "pipelineDefinition") -or ($_ -eq "pipelineExecution")}
+	{
+		[string]$IntegerOperation = Get-VstsInput -Name integerOperation -Require
+		if ($IntegerOperation -ne "none")
+		{
+			$Step = Get-VstsInput -Name step -Require
+			switch ($IntegerOperation)
+			{
+				"increment"
+				{
+					$newValue = $oldValue + $Step
+				}
+				"decrement"
+				{
+					$newValue = $oldValue - $Step
+				}
+			}
+		}
+	}
+	"customValue"
+	{
+		$newValue = Get-VstsInput -Name variableValue -Require
+	}
+}
+
+# Update the value and update Azure DevOps
+$projectDef.variables.$VariableName.Value = $newValue
+$projectDefJson = $projectDef | ConvertTo-Json -Depth 50 -Compress
+Write-Output ("projectDef.variables.VariableName.Value" -f $projectDef.variables.$VariableName.Value)
+
+# build the URL to cater for if the Project Definition URL already has parameters or not.
+$separator = "?"
+if ($projectDef.Url -like '*?*')
+{
+	$separator = "&"
+}
+$putUrl = "$($projectDef.Url)$($separator)api-version=$ApiVersion"
+Write-Verbose "Updating Project Build number with URL: $putUrl"
+Write-Output $projectDef.variables
+Write-Output (ConvertFrom-Json $projectDefJson)
+Write-Output "--------------------------"
+Invoke-RestMethod -Method Put -Uri $putUrl -Headers $headers -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($projectDefJson)) | Out-Null
+
+if ($OutputNewValue)
+{
+	Write-Host "##vso[task.setvariable variable=$VariableName]$newValue"
+}
+
+Trace-VstsLeavingInvocation $MyInvocation
